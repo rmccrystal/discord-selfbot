b7527398697eae82e11be240a672d01f5accb6a4
Ryan McCrystal <rrmccrystal@gmail.com>
2020-08-23 16:22:48 -1000

    Added support for generating a config if it doesn't exist

--- cmd/selfbot/main.go
+++ cmd/selfbot/main.go
@@ -1,25 +1,29 @@
 package main
 
 import (
+	"bufio"
+	"encoding/json"
 	"flag"
+	"fmt"
 	"github.com/rmccrystal/discord-selfbot/commands"
 	"github.com/rmccrystal/discord-selfbot/selfbot"
 	log "github.com/sirupsen/logrus"
 	"io/ioutil"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 )
 
 var (
-	debug      bool
-	configFile string
+	debug          bool
+	configFileName string
 )
 
 func main() {
 	// Parse flags
 	flag.BoolVar(&debug, "debug", false, "Enables debug mode")
-	flag.StringVar(&configFile, "config", "config.json", "Specifies the config file")
+	flag.StringVar(&configFileName, "config", "config.json", "Specifies the config file")
 
 	flag.Parse()
 
@@ -34,15 +38,41 @@ func main() {
 		log.SetLevel(log.DebugLevel)
 	}
 
-	// Open and read config file
-	configJson, err := ioutil.ReadFile(configFile)
-	if err != nil {
-		log.Fatalln(err)
-	}
+	// Generate or get config
+	var config selfbot.Config
+	if _, err := os.Stat(configFileName); os.IsNotExist(err) {
+		// does not exist, ask for token and generate a new file
+		reader := bufio.NewReader(os.Stdin)
+		log.Infof("Could not find config file %s, creating a new one", configFileName)
+		// TODO: add instructions for getting token
+		fmt.Print("Enter your token: ")
+		token, _ := reader.ReadString('\n')
+		token = strings.TrimSpace(token)
 
-	config, err := selfbot.NewConfigFromJson(configJson)
-	if err != nil {
-		log.Fatalf("Error parsing config file %s: %s", configFile, err)
+		// Create a new config
+		config = selfbot.NewConfigDefault(token)
+
+		// Save the new config file
+		configFile, err := os.Create(configFileName)
+		if err != nil {
+			panic(err)
+		}
+
+		enc := json.NewEncoder(configFile)
+		enc.SetEscapeHTML(false)
+		enc.SetIndent("", "  ")
+		_ = enc.Encode(config)
+	} else {
+		// Open and read config file
+		configJson, err := ioutil.ReadFile(configFileName)
+		if err != nil {
+			log.Fatalln(err)
+		}
+		config, err = selfbot.NewConfigFromJson(configJson)
+
+		if err != nil {
+			log.Fatalf("Error parsing config file %s: %s", configFileName, err)
+		}
 	}
 
 	commandList := commands.InitCommands()
